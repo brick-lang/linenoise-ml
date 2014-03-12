cf1acfc42185277bc41b78db8883c28098081347
Katherine Whitlock <toroidalcode@gmail.com>
2014-03-11 20:49:55 -0400

    Remove old externals

--- linenoise.ml
+++ linenoise.ml
@@ -1,21 +1,3 @@
-(* type linenoise_completions *)
-
-(* external linenoise_set_completion_callback: ((string -> linenoise_completions) -> unit) = "linenoise_set_completion_callback_c" *)
-
-(* external linenoise_add_completion: linenoise_completions -> string -> unit = "linenoise_add_completion_c" *)
-
-(* external linenoise: string -> string = "linenoise_c" *)
-(* external linenoise_history_add: string -> int = "linenoise_history_add_c" *)
-(* external linenoise_history_set_max_len: int -> int = "linenoise_history_set_max_len_c" *)
-(* external linenoise_history_save: string -> int = "linenoise_history_save_c" *)
-(* external linenoise_history_load: string -> int = "linenoise_history_load_c" *)
-(* external linenoise_clear_screen: unit -> unit = "linenoise_clear_screen_c" *)
-(* external linenoise_set_multiline: int -> unit = "linenoise_set_multiline_c" *)
-
-(* char* -> linenoiseCompletions* -> void *)
-(* string -> linenoise_completion -> unit *)
-
-
 open Ctypes
 open PosixTypes
 open Foreign
